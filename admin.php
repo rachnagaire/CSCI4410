947b1ac7a3ff04ef4de518cb795221eab96b89fe
Artemus Hayden <grayson2005hayden@gmail.com>
2025-04-22 12:39:16 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -13,6 +13,8 @@ $username = "root";
 $password = "";
 $database = "library_database"; 
 $conn = new mysqli($servername, $username, $password, $database);
+
+$flag = "userTable";
 ?>
 
 <!DOCTYPE html>
@@ -74,8 +76,8 @@ $conn = new mysqli($servername, $username, $password, $database);
                     $conn->query($delete_checkouts_sql);
 
                     //delete user
-                    $delete_sql = "DELETE FROM users WHERE user_id = $user_to_delete";
-                    if ($conn->query($delete_sql)) {
+                    $delete_user_sql = "DELETE FROM users WHERE user_id = $user_to_delete";
+                    if ($conn->query($delete_user_sql)) {
                         echo "<script>alert('User deleted successfully.');</script>";
                     } else {
                         echo "<script>alert('Failed to delete user.');</script>";
@@ -91,65 +93,158 @@ $conn = new mysqli($servername, $username, $password, $database);
             }
 
             if (isset($_POST['modBookData'])) {
-
+                $flag = "modBookData";
+                $all_books = "SELECT * FROM books ORDER BY title ASC, author ASC";
+                $default_result = $conn->query($all_books);
             }
 
+            if (isset($_POST['addBook'])) {
+                $title = $_POST['title'];
+                $author = $_POST['author'];
+                $description = $_POST['description'];
+                $length = $_POST['length'];
+                $genre = $_POST['genre'];
+                $image = $_POST['image'];
+                $copies = $_POST['copies_available'];
+            
+                $add_book_sql = "INSERT INTO books (title, author, description, length, genre, image, copies_available)
+                               VALUES ('$title', '$author', '$description', $length, '$genre', '$image', $copies)";
 
+                $dupe_title_check = "SELECT * FROM books WHERE title = '$title' AND author = '$author'";
+                $dupe_title_check_result = mysqli_query($conn, $dupe_title_check);
+
+                if ($dupe_title_check_result->num_rows > 0) {
+                    echo "<script>alert('Book already available.');</script>";
+                } elseif ($conn->query($add_book_sql)) {
+                        echo "<script>alert('Book added successfully.');</script>";
+                    } else {
+                        echo "<script>alert('Failed to add book. Make sure all fields are completed.');</script>";
+                }
+
+                $flag = "modBookData";
+                $all_books = "SELECT * FROM books ORDER BY title ASC, author ASC";
+                $default_result = $conn->query($all_books);
+            }
+
+            if (isset($_POST['deleteBook'])) {
+                $book_to_delete = $_POST['book_id'];
+
+                $delete_checkouts_sql = "DELETE FROM checked_out WHERE book_id = $book_to_delete";
+                $conn->query($delete_checkouts_sql);
+
+                $delete_book_sql = "DELETE FROM books WHERE book_id = $book_to_delete";
+                if ($conn->query($delete_book_sql)) {
+                    echo "<script>alert('Book deleted successfully.');</script>";
+                } else {
+                    echo "<script>alert('Failed to delete book.');</script>";
+                }
+                $flag = "modBookData";
+                $all_books = "SELECT * FROM books ORDER BY title ASC, author ASC";
+                $default_result = $conn->query($all_books);
+            }
         }
 
-        displayTable($default_result, $conn);
+        displayTable($default_result, $flag);
 
-        function displayTable($result) {
+        function displayTable($result, $flag) {
             if ($result){
-                //group users and their books
-                $users = [];
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $userId = $row['user_id'];
+                if ($flag == "userTable"){
+                    //group users and their books
+                    $users = [];
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $userId = $row['user_id'];
 
-                    if (!isset($users[$userId])) {
-                        $users[$userId] = [
-                            'user_id' => $userId,
-                            'username' => $row['username'],
-                            'email' => $row['email'],
-                            'books' => [],
-                        ];
-                    }
-
-                    if (!empty($row['book_title']) && !empty($row['book_author'])) {
-                        $users[$userId]['books'][] = "{$row['book_title']} by {$row['book_author']}";
-                    }
-                }
-
-                //display table
-                echo "<table border='1'>";
-                echo "<tr><th>User ID</th><th>Username</th><th>Email</th><th>Books Checked Out</th><th>Action</th></tr>";
-
-                foreach ($users as $user) {
-                    echo "<tr>";
-                    echo "<td>{$user['user_id']}</td>";
-                    echo "<td>{$user['username']}</td>";
-                    echo "<td>{$user['email']}</td>";
-
-                    echo "<td><ul>";
-                    if (!empty($user['books'])) {
-                        foreach ($user['books'] as $book) {
-                            echo "<li>$book</li>";
+                        if (!isset($users[$userId])) {
+                            $users[$userId] = [
+                                'user_id' => $userId,
+                                'username' => $row['username'],
+                                'email' => $row['email'],
+                                'books' => [],
+                            ];
                         }
-                    } else {
-                        echo "<li>No books checked out</li>";
+
+                        if (!empty($row['book_title']) && !empty($row['book_author'])) {
+                            $users[$userId]['books'][] = "{$row['book_title']} by {$row['book_author']}";
+                        }
                     }
-                    echo "</ul></td>";
 
-                    echo "<td>
-                        <form method='POST' onsubmit=\"return confirm('Are you sure you want to delete this user?');\">
-                            <input type='hidden' name='user_id' value='{$user['user_id']}'>
-                            <button type='submit' name='deleteUser'>Delete User</button>
-                        </form>
-                    </td>";
-                    echo "</tr>";
+                    //display table
+                    echo "<table border='1'>";
+                    echo "<tr><th>User ID</th><th>Username</th><th>Email</th><th>Books Checked Out</th><th>Action</th></tr>";
+
+                    foreach ($users as $user) {
+                        echo "<tr>";
+                        echo "<td>{$user['user_id']}</td>";
+                        echo "<td>{$user['username']}</td>";
+                        echo "<td>{$user['email']}</td>";
+
+                        echo "<td><ul>";
+                        if (!empty($user['books'])) {
+                            foreach ($user['books'] as $book) {
+                                echo "<li>$book</li>";
+                            }
+                        } else {
+                            echo "<li>No books checked out</li>";
+                        }
+                        echo "</ul></td>";
+
+                        echo "<td>
+                            <form method='POST' onsubmit=\"return confirm('Are you sure you want to delete this user?');\">
+                                <input type='hidden' name='user_id' value='{$user['user_id']}'>
+                                <button type='submit' name='deleteUser'>Delete User</button>
+                            </form>
+                        </td>";
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                } elseif ($flag == "modBookData"){
+                        echo "<h3>Add Book</h3>";
+                        echo "<form method='POST'>";
+                            echo "<tr>";
+                            echo "<td><input type='text' name='title' placeholder='Title'></td>";
+                            echo "<td><input type='text' name='author' placeholder='Author'></td>";
+                            echo "<td><input type='text' name='description' placeholder='Description'></td>";
+                            echo "<td><input type='number' name='length' placeholder='Pages'></td>";
+                            echo "<td><input type='text' name='genre' placeholder='Genre'></td>";
+                            echo "<td><input type='text' name='image' placeholder='Image URL'></td>";
+                            echo "<td><input type='number' name='copies_available' placeholder='Copies'></td>";
+                            echo "<td><button type='submit' name='addBook'>Add Book</button></td>";
+                            echo "</tr>";
+                        echo "</form>";
+                    
+                        echo "<br>";
+
+                    echo "<table border='1'>";
+                    echo "<tr>";
+                    echo "<th>" . "Title" . "</th>";
+                    echo "<th>" . "Author" . "</th>";
+                    echo "<th>" . "Description" . "</th>";
+                    echo "<th>" . "Page Count" . "</th>";
+                    echo "<th>" . "Genre" . "</th>";
+                    echo "<th>" . "Image" . "</th>";
+                    echo "<th>" . "Copies Available" . "</th>";
+                    echo "<th>" . "Action" . "</th>";
+                    echo "<tr>";
+                    
+                    while ($row = mysqli_fetch_assoc($result)) { 
+                        echo "<tr>";
+                        echo "<td>{$row['title']}</td>";
+                        echo "<td>{$row['author']}</td>";
+                        echo "<td>{$row['description']}</td>";
+                        echo "<td>{$row['length']}</td>";
+                        echo "<td>{$row['genre']}</td>";
+                        echo "<td>{$row['image']}</td>"; // echo "<td><img src='{$row['image']}'></td>"; -> replace with to display images
+                        echo "<td>{$row['copies_available']}</td>";
+                        echo "<td><form method='POST'>
+                                    <input type='hidden' name='book_id' value='{$row['book_id']}'>
+                                    <button type='submit' name='editBook'>Edit</button>
+                                    <button type='submit' name='deleteBook'>Delete</button>
+                        </form></td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
                 }
-
-                echo "</table>";
             }
         }
     
